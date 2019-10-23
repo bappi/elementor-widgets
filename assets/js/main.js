bbb638dadbdddc1594468c4567c4950bf16ec16f
kmasalam <39186564+kmasalam@users.noreply.github.com>
2019-10-23 13:12:36 +0600

    fun Factors all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,33 +105,7 @@
     // Counter Function 
 
 
-    jQuery('.advanced_blocks_funfactors').each(function() {
-        const __this = jQuery(this);
-            let countVal;
-            countVal = __this.html()
-             countVal = parseInt(countVal)
-            console.log(countVal)
-            function counterFunction(){
-                __this.prop('Counter', 0).animate({
-                    Counter: countVal
-                }, {
-                    duration: 2150,
-                    step: function (now, fx) {
-                        var data = Math.round(now);
-                        __this.html(data);
-                    }
-                })
-
-            }
-     
-            var self = __this;
-            __this.waypoint({
-                offset: '85%',
-                handler: function() {
-                     counterFunction()
-                }
-            });
-        })
+   
     // Progressbar
     jQuery('.advanced_block_progressbar_block').each(function() {
         const __this = jQuery(this)
