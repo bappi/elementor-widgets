81ed660ef77039b7061ee173f3d2e363fcccc829
kmasalam <39186564+kmasalam@users.noreply.github.com>
2019-11-26 20:16:31 +0600

    timeline complete with responsive

--- assets/js/newtimeline.js
+++ assets/js/newtimeline.js
@@ -3,11 +3,77 @@ $(document).ready(function(){
 	    var maxHeight = 0,maxElementHeight = null;
 
 	var maxHeight = function(elems){
-    return Math.max.apply(null, elems.map(function ()
-    {
-        return $(this).outerHeight();
-    }).get());
-}
+	    return Math.max.apply(null, elems.map(function ()
+	    {
+	        return $(this).outerHeight();
+	    }).get());
+	}
+	jQuery('.advanced_blocks_timeline.horizontal').each(function(index, el) {
+        var nextArrow,prevArrow,dotsClass,dots,arrows,slidesToShow,centerMode,autoplay,centerPadding;
+        if(jQuery(this).hasAttr('nextArrow')){
+            nextArrow = jQuery(this).attr('nextArrow');
+        }
+        if(jQuery(this).hasAttr('prevArrow')){
+            prevArrow = jQuery(this).attr('prevArrow');
+        }
+        if(jQuery(this).hasAttr('dotsClass')){
+            dotsClass = jQuery(this).attr('dotsClass');
+        }
+        if(jQuery(this).hasAttr('dots')){
+            dots = jQuery(this).attr('dots');
+        }
+        if(jQuery(this).hasAttr('arrows')){
+            arrows = jQuery(this).attr('arrows');
+            console.log('arrya is ',  arrows);
+        }
+        if(jQuery(this).hasAttr('slidesToShow')){
+            slidesToShow = jQuery(this).attr('slidesToShow');
+            slidesToShow = slidesToShow.split(' ')
+        }
+         if(jQuery(this).hasAttr('centerMode')){
+            centerMode = jQuery(this).attr('centerMode');
+        }
+         if(jQuery(this).hasAttr('centerPadding')){
+            centerPadding = jQuery(this).attr('centerPadding');
+            console.log(centerPadding , 'paddding')
+        }
+         if(jQuery(this).hasAttr('enableAutoPlay')){
+            autoplay = jQuery(this).attr('enableAutoPlay');
+            autoplay = autoplay === 'true' ? true : false
+            console.log('autoplay', autoplay)
+        }
+        jQuery(this).not('.slick-initialized').slick({
+            slidesToShow: slidesToShow ? parseInt(slidesToShow[0]) : 1,
+            centerPadding:  centerPadding ? centerPadding : '0px',
+            slidesToScroll: 1,
+            dots: dots === 'true' ? true : false,
+            centerMode: centerMode ? true : false,
+            arrows: arrows === 'true' ? true : false,
+            nextArrow: arrows === 'true' ? '<button type="button" class="slick-next">' + nextArrow + '</button>' : null,
+            prevArrow: arrows === 'true' ? '<button type="button" class="slick-prev">' + prevArrow + '</button>' : null,
+            dotsClass: dots === 'true' ? dotsClass : null,
+            autoplay: autoplay === 'true' ? autoplay : false,
+            autoplaySpeed: 3500,
+            responsive: [
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        arrows: true,
+			        slidesToShow: 3
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        arrows: true,
+			        slidesToShow: 3,
+			        vertical: true,
+			        verticalSwiping: true,
+			      }
+			    }
+			  ]
+        })
+    });
 	var actual = maxHeight($(".advanced_blocks_timeline.horizontal .slick-slide"));
 
 		if($(this).width() >= 576){
